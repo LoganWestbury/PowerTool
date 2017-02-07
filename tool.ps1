9fe294a65048abe561b330e0b3601bcb6a87ad5f
LoganWestbury <logan.westbury@gmail.com>
2017-02-07 14:22:41 +0000

    ADDED the function to actually show the menu

--- tool.ps1
+++ tool.ps1
@@ -25,6 +25,68 @@ function populateMainMenu
 	showMenuReusable -menuItems $menuItems
 }
 
+function showMenuReusable
+{
+	[CmdletBinding()]
+	Param (
+		[Parameter(Mandatory = $True)]
+		[Array]$menuItems
+	)
+	[Array]$tempMenuArray = $menuItems
+	
+	displayVersion
+	$menuCounter = $tempMenuArray.Length
+	$i = 0
+	
+	do
+	{
+		Write-Host ("Option $i : " + $tempMenuArray[$i])
+		[Environment]::NewLine
+		$i++
+	}
+	until ($i -eq $menuCounter)
+}
+
+function remoteShutdownChecker
+{
+	
+	
+	# Script for displaying scanning the event log of a remote PC and displaying when the PC has been shutdown/restarted.
+	# LW - 16/09/2016 
+	# Version 1.1
+	# Added - Displaying a counter for how many times the PC has been shutdown.
+	
+	# Begin Script
+	
+	$amountOfDays = 90
+	
+	# Setting the filter - saves resources by not scanning the entire event log
+	$FilterLog = @{
+		# Sets which part of the event log to search (Security, System etc...)
+		LogName = "System"
+		# Sets how far back in the log to check
+		StartTime = (Get-Date).AddDays(- $amountOfDays)
+		# Set the filter for the event log to the ID '6006' 
+		ID = 6006
+	}
+	
+	# Get User input for remote IP 
+	$remoteIP = Read-Host 'Input Remote IP / Terminal Name'
+	
+	# This section actually searches remote PC for shutdowns and puts the output into the variable
+	$Events = Get-WinEvent -ComputerName $remoteIP -FilterHashtable $FilterLog
+	
+	# Count the amount of times the event has happened. In this case it is how many times the PC has been shut down.
+	$amountOfRestarts = $Events.Count
+	
+	# Displaying how many times the PC has been restarted to console 
+	textSeperateLine -inputString "The PC with IP $remoteIP has been shut down $amountOfRestarts times in the last $amountOfDays days."
+	
+	# Display to console the output log
+	Get-WinEvent -ComputerName $remoteIP -FilterHashtable $FilterLog | Format-Table -AutoSize
+}
+
+
 function displayVersion
 {
 	Clear-Host
