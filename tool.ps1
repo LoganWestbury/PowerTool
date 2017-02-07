4bb210b239e7e2597ea8b7fcbf23db96d29ce35a
LoganWestbury <logan.westbury@gmail.com>
2017-02-07 14:05:33 +0000

    ADD function to wait for keypress
    ADD function to wait for keypress, can be used to wait for the user to press any button before removing any information from the screen

--- tool.ps1
+++ tool.ps1
@@ -1,2 +1,9 @@
 Import-Module ActiveDirectory
 
+function pressAnyKey
+{
+	# Wait for user to press any key to continue, gives user time to read output
+	textSeperateLine -inputString "Press any key to continue..."
+	$HOST.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") | OUT-NULL
+	$HOST.UI.RawUI.Flushinputbuffer()
+}
