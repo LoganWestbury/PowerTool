6fa1389b09d313a385028e747ca125868b0dbd32
LoganWestbury <logan.westbury@gmail.com>
2017-02-07 14:07:28 +0000

    ADD actual code for the menu switch case
    ADD actual code for the menu switch case

--- tool.ps1
+++ tool.ps1
@@ -24,3 +24,55 @@ function populateMainMenu
 	
 	showMenuReusable -menuItems $menuItems
 }
+
+do
+{
+	populateMainMenu
+	$input = Read-Host "Select an option"
+	
+	switch ($input)
+	{
+		'1' {
+			displayVersion
+			Write-Host ("Event Viewer Remote Shutdown Checker")
+			[Environment]::NewLine
+			remoteShutdownChecker
+			pressAnyKey
+			
+		} '2' {
+			displayVersion
+			scanADForLockedOutUsers
+			pressAnyKey
+			
+		} '3' {
+			displayVersion
+			Write-Host ("User Lockout Checker")
+			[Environment]::NewLine
+			Write-Host ("Known Issue - For every user you check for lockouts, you  must restart the application.") -ForegroundColor Yellow
+			[Environment]::NewLine
+			Write-Host ("May take about 30 seconds to complete. Do not press anything else until it has completed.") -ForegroundColor Yellow
+			[Environment]::NewLine
+			$identity = Read-Host 'Input User ID (e00 etc)'
+			[Environment]::NewLine
+			lockoutLocationFinder -Identity $identity
+			pressAnyKey
+			
+		} '4' {
+			displayVersion
+			Write-Host ("Active Directory Query User with Employee ID")
+			[Environment]::NewLine
+			$employeeID = Read-Host 'Input Employee ID (e00 etc)'
+			queryActiveDirectoryUser -employeeID $employeeID
+			pressAnyKey
+			
+		} '5' {
+			displayVersion
+			openRemoteDrive
+			pressAnyKey
+		} ('0')
+		{
+			return
+		}
+	}
+}
+until ($input -eq '0')
