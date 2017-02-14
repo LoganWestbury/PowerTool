c7f068dfbcc8d114db312f6e5080ec892e2efdfb
LoganWestbury <logan.westbury@gmail.com>
2017-02-14 11:05:56 +0000

    fixed menu links
    fixed menu links

--- tool.ps1
+++ tool.ps1
@@ -1,5 +1,7 @@
 Import-Module ActiveDirectory
 
+
+
 function pressAnyKey {
     # Wait for user to press any key to continue, gives user time to read output
     textSeperateLine -inputString "Press any key to continue..."
@@ -58,7 +60,7 @@ function createDesktopShortcut{
     )
 
 # Find logged in user
-
+findLoggedOnUser -ComputerName $inputHostName
 
   
 # Create desktop URL using logged in username 
@@ -71,7 +73,52 @@ function createDesktopShortcut{
 
 }
 
-
+function findLoggedOnUser { 
+       
+[CmdletBinding()]             
+ Param              
+   (                        
+    [Parameter(Mandatory=$true, 
+               Position=0,                           
+               ValueFromPipeline=$true,             
+               ValueFromPipelineByPropertyName=$true)]             
+    [String[]]$ComputerName 
+   )#End Param 
+ 
+Begin             
+{             
+ Write-Host "`n Checking Users . . . " 
+ $i = 0             
+}#Begin           
+Process             
+{ 
+    $ComputerName | Foreach-object { 
+    $Computer = $_ 
+    try 
+        { 
+            $processinfo = @(Get-WmiObject -class win32_process -ComputerName $Computer -EA "Stop") 
+                if ($processinfo) 
+                {     
+                    $processinfo | Foreach-Object {$_.GetOwner().User} |  
+                    Where-Object {$_ -ne "NETWORK SERVICE" -and $_ -ne "LOCAL SERVICE" -and $_ -ne "SYSTEM"} | 
+                    Sort-Object -Unique | 
+                    ForEach-Object { New-Object psobject -Property @{Computer=$Computer;LoggedOn=$_} } |  
+                    Select-Object Computer,LoggedOn 
+                }#If 
+        } 
+    catch 
+        { 
+            "Cannot find any processes running on $computer" | Out-Host 
+        } 
+     }#Forech-object(Comptuters)        
+             
+}#Process 
+End 
+{ 
+ 
+}#End 
+ 
+}#Get-LoggedOnUser 
 
 
 
@@ -537,7 +584,8 @@ function populateMenu_remoteTools {
         "Return",
         "Remote Shutdown Checker",
         "Open Remote PC's C:\ Drive",
-        "System Specs of Remote Host"
+        "System Specs of Remote Host",
+        "Find Logged On User"
     )
 	
     showMenuReusable -menuItems $menuItems
@@ -552,17 +600,22 @@ function displayMenu_remoteTools {
             '1' {
                 displayVersion
                 Write-Host("Remote Shutdown Checker")
-                
+                remoteShutdownChecker
                 pressAnyKey
             } '2' {
                 displayVersion
                 Write-Host("Open Remote PC's C:\ Drive")
-                
+                openRemoteDrive
                 pressAnyKey
             } '3' {
                 displayVersion
                 Write-Host("System Specs of Remote Host")
                 queryRemoteHost
+                pressAnyKey
+            } '4' {
+                displayVersion
+                Write-Host("Find Logged On User")
+                findLoggedOnUser
                 pressAnyKey
             } ('0') {
                 return
