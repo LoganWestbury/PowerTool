31b12e0f085ba33927e7b3f5fa7e7b160a4a43ec
LoganWestbury <logan.westbury@gmail.com>
2017-02-21 12:08:55 +0000

    Update remoteDesktop.ps1

--- remoteDesktop.ps1
+++ remoteDesktop.ps1
@@ -1,3 +1,5 @@
 $userInput = Read-Host("Enter remote host")
 
 $string1 = ""
+
+$string1 = Get-WmiObject Win32_ComputerSystem -ComputerName $userInput -recurse | Where-Object {!$_.PsIsContainer} | foreach {$_.Username}
