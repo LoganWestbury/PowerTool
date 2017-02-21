0cd3bac4330f7956e5fb93fb12a993bbe0b662c0
LoganWestbury <logan.westbury@gmail.com>
2017-02-21 12:09:35 +0000

    get the substring of the output

--- remoteDesktop.ps1
+++ remoteDesktop.ps1
@@ -3,3 +3,6 @@ $userInput = Read-Host("Enter remote host")
 $string1 = ""
 
 $string1 = Get-WmiObject Win32_ComputerSystem -ComputerName $userInput -recurse | Where-Object {!$_.PsIsContainer} | foreach {$_.Username}
+
+$string1 = $string1.Substring(3)
+
