b6c048c2936933e3cbfe039c5b5dbe5657d21eb6
LoganWestbury <logan.westbury@gmail.com>
2017-02-21 12:11:51 +0000

    get desktop location from username

--- remoteDesktop.ps1
+++ remoteDesktop.ps1
@@ -4,6 +4,9 @@ $string1 = ""
 
 $string1 = Get-WmiObject Win32_ComputerSystem -ComputerName $userInput -recurse | Where-Object {!$_.PsIsContainer} | foreach {$_.Username}
 
-$string1 = $string1.Substring(3)
+$string1 = $string1.Substring(4)
+
+$createDir = ("C:\Users\" + $string1 + "\Desktop\")
 
 Write-Host("Username: " + $string1)
+Write-Host("Directory: " + $createDir)
