f8af0880588f6605b4bc365b8c4389e6c380a637
LoganWestbury <logan.westbury@gmail.com>
2017-02-22 12:12:20 +0000

    comments

--- updateScripts.ps1
+++ updateScripts.ps1
@@ -2,27 +2,32 @@
 #   Created by Logan Westbury
 #   22/02/2017
 
+##########################################################
+#   Start the clock
+
 $start_time = Get-Date
-
-
 
 ##########################################################
 #   Download latest deployShortcut script 
 
 $url_deployShortcuts = "https://raw.githubusercontent.com/LoganWestbury/PowerTool/master/deployShortcuts.ps1"
 $output_deployShortcuts = ".\deployShortcuts.ps1"
+
+##########################################################
+#   Delete the file if it already exists
+
 If (Test-Path $output_deployShortcuts){
 	Remove-Item $output_deployShortcuts
 }
 
+##########################################################
+#   Download the file from the given url
 Invoke-WebRequest -Uri $url_deployShortcuts -OutFile $output_deployShortcuts 
 
-
-
-
+##########################################################
+#   Display the output
 
 [Environment]::NewLine
 Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
-Read-Line("Press any key to exit...")
 [Environment]::NewLine
-
+Read-Host("Press any key to exit...")
