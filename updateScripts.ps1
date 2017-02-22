d017915e445de75d0c0e56446fd4e0592814980e
LoganWestbury <logan.westbury@gmail.com>
2017-02-22 11:53:32 +0000

    works

--- updateScripts.ps1
+++ updateScripts.ps1
@@ -1,3 +1,28 @@
 ##########################################################
 #   Created by Logan Westbury
 #   22/02/2017
+
+$start_time = Get-Date
+
+
+
+##########################################################
+#   Download latest deployShortcut script 
+
+$url_deployShortcuts = "https://raw.githubusercontent.com/LoganWestbury/PowerTool/master/deployShortcuts.ps1"
+$output_deployShortcuts = ".\deployShortcuts.ps1"
+If (Test-Path $output_deployShortcuts){
+	Remove-Item $output_deployShortcuts
+}
+
+Invoke-WebRequest -Uri $url_deployShortcuts -OutFile $output_deployShortcuts 
+
+
+
+
+
+[Environment]::NewLine
+Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+Read-Line("Press any key to exit...")
+[Environment]::NewLine
+
