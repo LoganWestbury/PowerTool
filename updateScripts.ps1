1029db7088db89dbbfb3883d548b8d2152530470
LoganWestbury <logan.westbury@gmail.com>
2017-02-22 12:15:25 +0000

    Update updateScripts.ps1

--- updateScripts.ps1
+++ updateScripts.ps1
@@ -7,22 +7,67 @@
 
 $start_time = Get-Date
 
+
+
 ##########################################################
-#   Download latest deployShortcut script 
+#   Download and deploy latest updateScripts script 
+
+$url_updateScripts = "https://raw.githubusercontent.com/LoganWestbury/PowerTool/master/updateScripts.ps1"
+$output_updateScripts = ".\updateScripts.ps1"
+
+#   Delete the file if it already exists
+If (Test-Path $output_updateScripts){
+	Remove-Item $output_updateScripts
+}
+
+#   Download the file from the given url
+Invoke-WebRequest -Uri $url_updateScripts -OutFile $output_updateScripts 
+
+
+
+
+
+
+
+
+
+
+##########################################################
+#   Download and deploy latest deployShortcut script 
 
 $url_deployShortcuts = "https://raw.githubusercontent.com/LoganWestbury/PowerTool/master/deployShortcuts.ps1"
 $output_deployShortcuts = ".\deployShortcuts.ps1"
 
-##########################################################
 #   Delete the file if it already exists
-
 If (Test-Path $output_deployShortcuts){
 	Remove-Item $output_deployShortcuts
 }
 
-##########################################################
 #   Download the file from the given url
 Invoke-WebRequest -Uri $url_deployShortcuts -OutFile $output_deployShortcuts 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ##########################################################
 #   Display the output
@@ -31,5 +76,3 @@ Invoke-WebRequest -Uri $url_deployShortcuts -OutFile $output_deployShortcuts
 Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 [Environment]::NewLine
 Read-Host("Press any key to exit...")
-
-#   Testing the auto update function of this file 
