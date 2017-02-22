5534d7551e791bb3de2d2b3648c1b766226c855e
LoganWestbury <logan.westbury@gmail.com>
2017-02-22 13:00:08 +0000

    Update deployShortcuts.ps1

--- deployShortcuts.ps1
+++ deployShortcuts.ps1
@@ -44,26 +44,26 @@ $createDir = ("\\$userInput\C$\Users\$string1\Desktop\Shortcuts\")
 ##########################################################
 #   Copy over the browser specific shortcuts
 
-Copy-Item -Path (".\resources\Shortcuts\Salesmans Office.lnk") -Destination ("$createDir\") 
-Copy-Item -Path (".\resources\Shortcuts\AC Web Mail.lnk") -Destination ("$createDir\") 
-Copy-Item -Path (".\resources\Shortcuts\ASO 2.lnk") -Destination ("$createDir\") 
-Copy-Item -Path (".\resources\Shortcuts\ASO 2_5.lnk") -Destination ("$createDir\") 
+Copy-Item -Path ("$PSScriptRoot\resources\Shortcuts\Salesmans Office.lnk") -Destination ("$createDir\") 
+Copy-Item -Path ("$PSScriptRoot\resources\Shortcuts\AC Web Mail.lnk") -Destination ("$createDir\") 
+Copy-Item -Path ("$PSScriptRoot\resources\Shortcuts\ASO 2.lnk") -Destination ("$createDir\") 
+Copy-Item -Path ("$PSScriptRoot\Shortcuts\ASO 2_5.lnk") -Destination ("$createDir\") 
 
 ##########################################################
 #   Copy over the Rev8 shortcuts
 
-Copy-Item -Path (".\resources\Shortcuts\Rev8 (Kerridge)\Rev8 Arnold Clark.kcc") -Destination ("$createDir\Rev8 (Kerridge)\") 
-Copy-Item -Path (".\resources\Shortcuts\Rev8 (Kerridge)\Rev8 Harry Fairbairn.kcc") -Destination ("$createDir\Rev8 (Kerridge)\") 
-Copy-Item -Path (".\resources\Shortcuts\Rev8 (Kerridge)\Rev8 John R Weir.kcc") -Destination ("$createDir\Rev8 (Kerridge)\") 
+Copy-Item -Path ("$PSScriptRoot\Shortcuts\Rev8 (Kerridge)\Rev8 Arnold Clark.kcc") -Destination ("$createDir\Rev8 (Kerridge)\") 
+Copy-Item -Path ("$PSScriptRoot\resources\Shortcuts\Rev8 (Kerridge)\Rev8 Harry Fairbairn.kcc") -Destination ("$createDir\Rev8 (Kerridge)\") 
+Copy-Item -Path ("$PSScriptRoot\resources\Shortcuts\Rev8 (Kerridge)\Rev8 John R Weir.kcc") -Destination ("$createDir\Rev8 (Kerridge)\") 
 
 ##########################################################
 #   Copy over frequently used backup URLs
 #   These will open up in the default browser
 
-Copy-Item -Path (".\resources\Shortcuts\Backup Links\ASO2.url") -Destination ("$createDir\Backup Links\") 
-Copy-Item -Path (".\resources\Shortcuts\Backup Links\ASO2_5.url") -Destination ("$createDir\Backup Links\") 
-Copy-Item -Path (".\resources\Shortcuts\Backup Links\WebMail.url") -Destination ("$createDir\Backup Links\") 
-Copy-Item -Path (".\resources\Shortcuts\Backup Links\Salesmans_Office.url") -Destination ("$createDir\Backup Links\") 
+Copy-Item -Path ("$PSScriptRoot\resources\Shortcuts\Backup Links\ASO2.url") -Destination ("$createDir\Backup Links\") 
+Copy-Item -Path ("$PSScriptRoot\resources\Shortcuts\Backup Links\ASO2_5.url") -Destination ("$createDir\Backup Links\") 
+Copy-Item -Path ("$PSScriptRoot\resources\Shortcuts\Backup Links\WebMail.url") -Destination ("$createDir\Backup Links\") 
+Copy-Item -Path ("$PSScriptRoot\resources\Shortcuts\Backup Links\Salesmans_Office.url") -Destination ("$createDir\Backup Links\") 
 
 ##########################################################
 #   Open explorer at the new directory
